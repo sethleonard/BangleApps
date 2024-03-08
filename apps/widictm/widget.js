7e9183b1bebeeaa237ec52aacac7a5f5a1321c92
Seth Leonard <20046237+sethleonard@users.noreply.github.com>
2024-03-07 19:34:53 -0800

    Add files via upload

---
+++ apps/widictm/widget.js
@@ -0,0 +1,36 @@
+require("Font8x16").add(Graphics);
+
+(() => {
+  var width = 35; // width of the widget
+  
+  function draw() {
+    g.reset(); // reset the graphics context to defaults (color/font/etc)
+//    g.setFontAlign(0,0); // center fonts    
+    g.setFont("8x16");
+    // Use 'locale' module to get a shortened month name
+    // in the correct language    
+    var text = "ICTM";
+    
+    let color = '#';
+    for (let i = 0; i < 3; i++) {
+      color += Math.random() < 0.5 ? '00' : 'FF';
+    }
+    
+    g.setColor(color);
+    
+    g.drawRect(this.x+2, this.y+2, this.x+width-1, this.y+19); // check the bounds!
+    
+    g.drawString(text, this.x+5, this.y+4);
+  }
+
+  setInterval(function() {
+    WIDGETS["widictm"].draw(WIDGETS["widictm"]);
+  }, 10*60000); // update every 10 minutes
+
+  // add your widget
+  WIDGETS["widictm"]={
+    area:"tl", // tl (top left), tr (top right), bl (bottom left), br (bottom right), be aware that not all apps support widgets at the bottom of the screen
+    width: width, // how wide is the widget? You can change this and call Bangle.drawWidgets() to re-layout
+    draw:draw // called to draw the widget
+  };
+})()
